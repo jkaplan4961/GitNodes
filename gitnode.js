71a4151bd4eb744380732fb2319cdba96e0ccdaf
Jeff Kaplan <58373667+jkaplan4961@users.noreply.github.com>
2021-01-21 20:33:09 -0500

    added more notes

--- gitnode.js
+++ gitnode.js
@@ -2,6 +2,22 @@
 // git is a version control system used to track changes in your files.
 // Git commits are checksumed usiing sha-1, meaning no corrupt files go unoticed without users knowledge
 // Three states of Git
--modified
+// -modified
 // file changed, but it has not been commited
-// staginggit log
+// staging
+// files that are marked to go into the next commit snapshot by using git add .
+// commit state
+// data safely stored in the local database
+// 
+// to start version/track control
+// git init
+// creates new project
+// your project should never be inside another project folder in order to avoid inverse tree tracking issues
+//
+// stage our files for a commit
+// git add
+
+// use git status to check on file staging
+
+// to commit your files, use git commit -m " Your message here"
+
