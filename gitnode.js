b4d605fedee7292dde96a1c6b7ce022b17a4e289
Jeff Kaplan <58373667+jkaplan4961@users.noreply.github.com>
2021-01-21 20:19:32 -0500

    test

---
+++ gitnode.js
@@ -0,0 +1,7 @@
+// Git
+// git is a version control system used to track changes in your files.
+// Git commits are checksumed usiing sha-1, meaning no corrupt files go unoticed without users knowledge
+// Three states of Git
+-modified
+// file changed, but it has not been commited
+// staginggit log
